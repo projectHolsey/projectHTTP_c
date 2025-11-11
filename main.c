77d92f27ce15bc24d148eac68f90cd4e58326efa
projectholsey <matty-sep@hotmail.com>
2025-11-11 15:33:58 -0500

    Updating code to read and print 8 bytes at a time from the file

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ int main() {
     FILE *filePointer;
     char filename[] = "messages.txt";
 
-    char buffer[512]; // Character buffer to read the file into
+    char lineBuffer[8]; // Character buffer to read the file into, 8 bytes at a time
+    char fileBuffer[512];
 
     // Open the file in read mode
     filePointer = fopen(filename, "r");
@@ -21,8 +22,12 @@ int main() {
 
     // fgets: gets next char from specified stream (unsigned char) and advance ptr postion
     // In this case, we want to read sizeof(buffer) from filePointer stream
-    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
-        printf("%s", buffer); // Print each line read
+    while (fgets(lineBuffer, sizeof(lineBuffer), filePointer) != NULL) {
+        printf("%s\n", lineBuffer); // Print each line read
+        // strncat is just concatinate the one strinng onto the end of another.
+        // this function allows the 3rd argument to specify the amount of characters we want to copy over
+        //          note - there is another function 'strcat' that doesn't specify size and just does it. 
+        strncat(fileBuffer, lineBuffer, sizeof(lineBuffer)); 
     }
 
     return 0;
