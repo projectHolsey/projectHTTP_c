8d051af7a23a77c83e68fb6d3c7947661ef84177
projectholsey <matty-sep@hotmail.com>
2025-11-11 19:32:34 -0500

    Updated the code to do the file read in a separate function

--- main.c
+++ main.c
@@ -2,16 +2,37 @@
 #include <string.h>
 
 
+char *fileReader(FILE *fileStream) {
+
+    printf("I got to here before something went wrong\n");
+
+    char lineBuffer[8]; // Character buffer to read the file into, 8 bytes at a time
+    char fileBuffer[512];
+    
+    // fgets: gets next char from specified stream (unsigned char) and advance ptr postion
+    // In this case, we want to read sizeof(buffer) from filePointer stream
+    while (fgets(lineBuffer, sizeof(lineBuffer), fileStream) != NULL) {
+
+        // printf("%s\n", lineBuffer); // Print each line read
+
+        // strncat is just concatinate the one strinng onto the end of another.
+        // this function allows the 3rd argument to specify the amount of characters we want to copy over
+        //          note - there is another function 'strcat' that doesn't specify size and just does it. 
+        strncat(fileBuffer, lineBuffer, sizeof(lineBuffer)); 
+    }  
+    
+    printf("The contents of the file were: \n\n%s", fileBuffer);
+
+    return fileBuffer;
+}
+
 
 int main() {
 
     // Create a new file Stream object
     FILE *filePointer;
     char filename[] = "messages.txt";
-
-    char lineBuffer[8]; // Character buffer to read the file into, 8 bytes at a time
-    char fileBuffer[512];
-
+   
     // Open the file in read mode
     filePointer = fopen(filename, "r");
  
@@ -20,15 +41,9 @@ int main() {
         return 1; // 1 being an error
     }
 
-    // fgets: gets next char from specified stream (unsigned char) and advance ptr postion
-    // In this case, we want to read sizeof(buffer) from filePointer stream
-    while (fgets(lineBuffer, sizeof(lineBuffer), filePointer) != NULL) {
-        printf("%s\n", lineBuffer); // Print each line read
-        // strncat is just concatinate the one strinng onto the end of another.
-        // this function allows the 3rd argument to specify the amount of characters we want to copy over
-        //          note - there is another function 'strcat' that doesn't specify size and just does it. 
-        strncat(fileBuffer, lineBuffer, sizeof(lineBuffer)); 
-    }
+    char *returnVal = fileReader(filePointer);
+
+    printf("%s", returnVal);
 
     return 0;
 }
